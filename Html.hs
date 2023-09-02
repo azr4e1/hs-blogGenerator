9ae120bf44db499d6ae4610bc8d7f233ee644e62
Lorenzo Drumond <drumondlorenzo@posteo.net>
2023-09-02 23:18:23 +0100

    Created Internal module for Html library

--- Html.hs
+++ Html.hs
@@ -9,46 +9,4 @@ module Html
      render)
 where
 
-newtype Html = Html String
-newtype Structure = Structure String
-type Title = String
-
-html_ :: Title -> Structure -> Html
-html_ title (Structure body) = Html .
-    el "html" $
-    el "head" ( el "title" . escape $ title)
-    ++ el "body" body
-
-el :: String -> String -> String
-el tag content =
-    "<" ++ tag ++ ">" ++ content ++ "</" ++ tag ++ ">"
-
-p_ :: String -> Structure
-p_ = Structure . el "p" . escape
-
-h1_ :: String -> Structure
-h1_ = Structure . el "h1" . escape
-
-append_ :: Structure -> Structure -> Structure
-append_ (Structure str1) (Structure str2) =
-    Structure (str1 ++ str2)
-
-getStructureString :: Structure -> String
-getStructureString (Structure struct) = struct
-
-render :: Html -> String
-render (Html str) = str
-
-escape :: String -> String
-escape =
-    let
-        escapeChar c =
-            case c of
-                '<' -> "&lt;"
-                '>' -> "&gt;"
-                '&' -> "&amp;"
-                '"' -> "&quot;"
-                '\'' -> "&#39;"
-                _ -> [c]
-    in
-        concatMap escapeChar
+import Html.Internal
