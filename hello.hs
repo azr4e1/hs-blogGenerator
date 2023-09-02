111411872365940b80126dcf844aae22ceab4568
Lorenzo Drumond <drumondlorenzo@posteo.net>
2023-09-02 22:40:29 +0100

    Created minimal EDSL

--- hello.hs
+++ hello.hs
@@ -1,30 +1,55 @@
 main :: IO ()
-main = putStrLn myHtml
+main = putStrLn . render $ myHtml
 
-myHtml :: String
-myHtml = makeHTML "My Page" (h1_ "How" ++ p_ "Are you?")
+myHtml :: Html
+myHtml = html_ "My title"
+    (h1_ "Heading" `append_`
+        (p_ "Paragraph #1" `append_` p_ "Paragraph #2"))
+
+newtype Html = Html String
+newtype Structure = Structure String
+type Title = String
+
+html_ :: Title -> Structure -> Html
+html_ title (Structure body) = Html .
+    el "html" $
+    el "head" ( el "title" title)
+    ++ el "body" body
 
 el :: String -> String -> String
 el tag content =
     "<" ++ tag ++ ">" ++ content ++ "</" ++ tag ++ ">"
-html_ :: String -> String
-html_  = el "html"
 
-body_ :: String -> String
-body_  = el "body"
 
-head_ :: String -> String
-head_ = el "head"
+-- body_ :: String -> Structure
+-- body_  = Structure . el "body"
+--
+-- head_ :: String -> Structure
+-- head_ = Structure . el "head"
+--
+-- title_ :: String -> Structure
+-- title_ = Structure . el "title"
 
-title_ :: String -> String
-title_ = el "title"
+p_ :: String -> Structure
+p_ = Structure . el "p"
 
-p_ :: String -> String
-p_ = el "p"
+h1_ :: String -> Structure
+h1_ = Structure . el "h1"
 
-h1_ :: String -> String
-h1_ = el "h1"
+append_ :: Structure -> Structure -> Structure
+append_ (Structure str1) (Structure str2) =
+    Structure (str1 ++ str2)
 
-makeHTML :: String -> String -> String
-makeHTML title content = html_ body
-    where body = head_ (title_ title) ++ body_ content
+
+-- makeHTML :: String -> String -> Html
+-- makeHTML title content = html_ body
+--     where body = head_ (title_ title) `append_` body_ content
+
+
+-- getStructureString :: Structure -> String
+-- getStructureString struct =
+--     case struct of
+--         Structure str -> str
+
+render :: Html -> String
+render (Html str) = str
