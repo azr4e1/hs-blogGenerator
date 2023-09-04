b981748938fa0da0edd0d5f338db49dd87689f62
Lorenzo Drumond <drumondlorenzo@posteo.net>
2023-09-04 01:13:46 +0100

    Refactored Main.hs to use do notation

--- Main.hs
+++ Main.hs
@@ -5,45 +5,48 @@ import System.Directory (doesFileExist)
 import System.Environment (getArgs)
 
 confirm :: IO Bool
-confirm =
-    putStr "The output file already exist. Are you sure you want to overwrite it? (y/n): " *>
-        getLine >>= \answer ->
-            case answer of
-                "y" -> pure True
-                "n" -> pure False
-                _ ->
-                    putStrLn "Invalid response. Use y or n" *>
-                    confirm
-
+confirm = do
+    putStr "The output file already exist. Are you sure you want to overwrite it? (y/n): "
+    answer <- getLine
+    case answer of
+        "y" -> pure True
+        "n" -> pure False
+        _ ->
+            putStrLn "Invalid response. Use y or n" *>
+            confirm
+    
 whenIO :: IO Bool -> IO () -> IO ()
-whenIO cond action =
-    cond >>= \result ->
-        if result
-            then action
-            else pure ()
+whenIO cond action = do
+    result <- cond
+    if result
+        then action
+        else pure ()
 
 readWriteParsedContent :: FilePath -> FilePath -> IO ()
-readWriteParsedContent input output =
-    readFile input >>= \content ->
-        whenIO (doesFileExist output >>= \result -> if result then confirm else pure True) .
-            writeFile output .
-            Convert.process input $
-            content
+readWriteParsedContent input output = do
+    content <- readFile input
+    outputExists <- doesFileExist output
+    whenIO (if outputExists then confirm else pure True) .
+        writeFile output .
+        Convert.process input $
+        content
 
 main :: IO ()
-main =
-    getArgs >>=
-    \arguments ->
-        case arguments of
-            [] -> getContents >>= print . Convert.process "Standard Input"
-            [input] -> 
-                doesFileExist input >>=
-                    \result ->
-                        readFile input >>= \content -> 
-                            if result then print . Convert.process input $ content
-                                      else putStrLn "The input file provided does not exist."
-            [input, output] ->
-                doesFileExist input >>=
-                    \result -> if result then readWriteParsedContent input output
-                                         else putStrLn "The input file provided does not exist."
-            _ -> putStrLn "Usage: runghc Main.hs [-- <input-file> <output-file>]"
+main = do
+    arguments <- getArgs
+    case arguments of
+        [] -> do
+            content <- getContents
+            print . Convert.process "Standard Input" $ content
+        [input] -> do
+            inputExists <- doesFileExist input
+            if inputExists then do
+                content <- readFile input
+                print . Convert.process input $ content
+            else putStrLn "The input file provided does not exist."
+        [input, output] -> do
+            inputExists <- doesFileExist input
+            if inputExists then readWriteParsedContent input output
+            else putStrLn "The input file provided does not exist."
+        _ -> putStrLn "Usage: runghc Main.hs [-- <input-file> <output-file>]"
+
